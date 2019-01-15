57c3a111cc0de0c69a76ce591deb045cd17d2644
oleg <s2ferr@gmail.com>
2019-01-15 12:35:17 +0300

    clear question stuff after delete

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -1,8 +1,12 @@
-import _ from "lodash"
-import CONST from "../const"
-import mongoose from "mongoose"
-import mongoose_delete from "mongoose-delete"
-mongoose.Promise = require("bluebird")
+import _ from 'lodash'
+import CONST from '../const'
+import Vote from '../models/vote.model'
+import Tag from '../models/tag.model'
+import Mark from '../models/mark.model'
+import Comment from '../models/comment.model'
+import mongoose from 'mongoose'
+import mongoose_delete from 'mongoose-delete'
+mongoose.Promise = require('bluebird')
 
 const Schema = mongoose.Schema
 
@@ -17,27 +21,27 @@ const countersSchema = new Schema(
 
 const questionSchema = new Schema(
 	{
-		owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 		title: { type: String, required: true },
 		uri: { type: String, required: true },
 		keywords: { type: [String], default: [], index: true },
 		votes: {
-			type: [{ type: Schema.Types.ObjectId, ref: "Vote" }],
+			type: [{ type: Schema.Types.ObjectId, ref: 'Vote' }],
 			default: [],
 			select: false
 		},
 		tags: {
-			type: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+			type: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
 			default: [],
 			select: false
 		},
 		marks: {
-			type: [{ type: Schema.Types.ObjectId, ref: "Mark" }],
+			type: [{ type: Schema.Types.ObjectId, ref: 'Mark' }],
 			default: [],
 			select: false
 		},
 		comments: {
-			type: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+			type: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 			default: [],
 			select: false
 		},
@@ -53,17 +57,17 @@ const questionSchema = new Schema(
 	}
 )
 
-questionSchema.index({ title: "text", keywords: "text" })
+questionSchema.index({ title: 'text', keywords: 'text' })
 
 questionSchema.statics.createQuestion = ({
 	owner,
-	title = "",
+	title = '',
 	keywords = []
 }) => {
 	// add "?" symbol
 	title = _.trim(title)
 	if (!/\?$/.test(title)) {
-		title = title + "?"
+		title = title + '?'
 	}
 
 	return getUri(title).then(uri => {
@@ -77,12 +81,34 @@ questionSchema.statics.createQuestion = ({
 	})
 }
 
+questionSchema.statics.deleteQuestion = id => {
+	return ModelClass.findById(id).then(question => {
+		if (!question) {
+			return Promise.reject('Not found')
+		}
+		return Tag.find({ question })
+			.remove()
+			.then(() => {
+				return Vote.find({ question }).remove()
+			})
+			.then(() => {
+				return Mark.find({ question }).remove()
+			})
+			.then(() => {
+				return Comment.find({ question }).remove()
+			})
+			.then(() => {
+				return question.delete()
+			})
+	})
+}
+
 function getUri(title) {
-	let p = title.replace(/[^a-zA-Z0-9\s]/g, "")
-	let uri = encodeURIComponent(p.replace(/\s+/g, "-"))
+	let p = title.replace(/[^a-zA-Z0-9\s]/g, '')
+	let uri = encodeURIComponent(p.replace(/\s+/g, '-'))
 	return ModelClass.findOne({ uri }).then(model => {
 		if (model) {
-			return getUri(title + " " + _.random(0, 100))
+			return getUri(title + ' ' + _.random(0, 100))
 		} else {
 			return uri
 		}
@@ -93,5 +119,5 @@ questionSchema.plugin(mongoose_delete, {
 	deletedAt: true,
 	overrideMethods: true
 })
-const ModelClass = mongoose.model("Question", questionSchema)
+const ModelClass = mongoose.model('Question', questionSchema)
 export default ModelClass
